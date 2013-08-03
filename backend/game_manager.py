c1ad60db5dac4543b981ee804d01a86fd54c6482
Kevin Dungs <kevin@kads24.eu>
2013-08-03 18:45:56 +0200

    Fix merge conflict.

--- backend/game_manager.py
+++ backend/game_manager.py
@@ -16,7 +16,7 @@ class GameManager(object):
         """
         """
         self._lab_name = lab_name
-        self._data_centre = technology.from_tech_tree('datacentre', 0)  # working on this
+        self._data_centre = technology.from_tech_tree('datacentre', 0)
         self._accelerator = technology.from_tech_tree('accelerator', accelerator_geometry, accelerator_particles, 0)
         self._funds = settings.INITIAL_FUNDS - self._accelerator.price
         self._hr_manager = HR(self._accelerator.num_scientists)
