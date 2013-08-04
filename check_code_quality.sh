928565be5063d34b64e1501147425893300eb758
Kevin Dungs <kevin@kads24.eu>
2013-08-04 10:07:20 +0200

    Add javascript checking via jslint.

--- check_code_quality.sh
+++ check_code_quality.sh
@@ -6,3 +6,11 @@ do
     pyflakes-2.7 $f
 done
 echo "Done."
+
+echo "Checking quality of frontend javascript code with jslint."
+for f in `find . -name "*.js" -not -name "*.min.js"`
+do
+    echo $f
+    jslint $f
+done
+echo "Done."
