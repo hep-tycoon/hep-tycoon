dd2d8decc410256d661bafde6214a038efb2c733
Kevin Dungs <kevin@kads24.eu>
2013-08-04 11:21:03 +0200

    Set up branch for presentation. Add tags.

--- backend/level.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-from os import path
-
-Level = namedtuple('Level', ['publication_target', 'grant', 'discovery'])
-
-levels = None
-with open(path.join(path.dirname(__file__), 'levels.json')) as level_file:
-    from json import load
-    levels = [Level(**x) for x in load(level_file)]
