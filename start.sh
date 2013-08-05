a5a4cd941932e39852dbb8a523fc317c9a0c5c06
Lennaert Bel <lennaert.bel@gmail.com>
2013-08-05 11:21:55 +0200

    Argument to start.sh is now appended to the opened url.
    This allow you to immediately start a new game, e.g. by running
     ./start.sh "/init_game/linear/ee/test"

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@
 rm backend/*.pyc
 
 # beta (todo: find the correct python version)
-(sleep 2 && open http://localhost:5000) &
+(sleep 2 && open "http://localhost:5000$1") &
 echo "Gameserver starting. Stop with ctrl-c."
 python2.7 testserver.py
